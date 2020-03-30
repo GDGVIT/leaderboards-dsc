91a073889c9d4f95f24fa62b9687f91dbe13952e
Ashutosh Kaushik <ash@pop-os.localdomain>
2020-03-30 23:05:41 +0530

    all bug fixes

---
+++ src/admin.js
@@ -0,0 +1,14 @@
+import React, {useEffect} from 'react'
+
+const Admin = () => {
+    // useEffect({
+    //     console.log("hey")
+    // })
+    return(
+        <div>
+            
+        </div>
+    );
+}
+
+export default Admin;
